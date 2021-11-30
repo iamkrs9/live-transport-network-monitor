8927b0ea9f823b02149c0154076e8d8daa55784d
Karan Shah <karanshah@Karans-MBP.home>
2021-11-29 22:58:16 -0500

    Connect to echo WebSocket server (sync)

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,43 @@
 #include <boost/asio.hpp>
 #include <boost/system/error_code.hpp>
 #include <iostream>
-#include <iomanip>
-#include <thread>
+#include <boost/beast.hpp>
 
 using tcp = boost::asio::ip::tcp;
+namespace websocket = boost::beast::websocket;
 
 void Log(boost::system::error_code ec) {
 	std::cerr << (ec ? "Error" : "OK") << (ec ? ec.message() : "") << std::endl;
 }
 
-void OnConnect(boost::system::error_code ec) {
-	std::cerr << "[" << std::setw(14) << std::this_thread::get_id() << "] ";
-	Log(ec);
-}
-
 int main() {
-	std::cerr << "[" << std::setw(14) << std::this_thread::get_id() << "] main" << std::endl;
 
 	boost::asio::io_context ioc{};
 
-	tcp::socket socket{boost::asio::make_strand(ioc)};
-	
-	size_t nThreads{4};
+	tcp::socket socket{ioc};
 
 	boost::system::error_code ec{};
 	tcp::resolver resolver{ioc};
-	auto resolverIt{resolver.resolve("google.com", "80", ec)};
+	auto resolverIt{resolver.resolve("ltnm.learncppthroughprojects.com", "80", ec)};
 
 	if(ec) {
 		Log(ec);
 		return -1;
 	}
-	for(size_t idx{0}; idx < nThreads; idx++) {	
-		socket.async_connect(*resolverIt, OnConnect);
+	socket.connect(*resolverIt, ec);
+	if(ec) {
+		Log(ec);
+		return -2;
 	}
-
-	std::vector<std::thread> threads{};
-	threads.reserve(nThreads);
-	for(size_t idx{0}; idx < nThreads; ++idx) {
-		threads.emplace_back([&ioc]() {
-			ioc.run();
-		});
-	}
-	for(size_t idx{0}; idx< nThreads; ++idx) {
-		threads[idx].join();
-	}
-
+	Log(ec);
+	websocket::stream<boost::beast::tcp_stream> ws(std::move(socket));
+	ws.handshake("ltnm.learncppthroughprojects.com", "/echo", ec);
+	Log(ec);
+	boost::asio::const_buffer b("ssup", sizeof("ssup"));
+	ws.write(b, ec);
+	Log(ec);
+	boost::beast::flat_buffer b1{};
+	ws.read(b1, ec);
+	std::cout << boost::beast::make_printable(b1.data()) << '\n';
 	return 0;
 }
