ecdc2233cd7efd9bdc4b88e1514b7d87b13b0f77
Karan Shah <karanshah@Karans-MBP.home>
2021-11-30 22:12:39 -0500

    Use the Boost asynchronous APIs

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <boost/system/error_code.hpp>
 #include <iostream>
 #include <boost/beast.hpp>
+#include <string>
 
 using tcp = boost::asio::ip::tcp;
 namespace websocket = boost::beast::websocket;
@@ -10,34 +11,76 @@ void Log(boost::system::error_code ec) {
 	std::cerr << (ec ? "Error" : "OK") << (ec ? ec.message() : "") << std::endl;
 }
 
+void OnReceive(boost::beast::flat_buffer &b1, boost::system::error_code &ec) {
+	if(ec) {
+		Log(ec);
+		return;
+	}
+	std::cout << boost::beast::make_printable(b1.data()) << '\n';
+}
+
+void OnSend(websocket::stream<boost::beast::tcp_stream> &ws, boost::beast::flat_buffer &b1, boost::system::error_code &ec) {
+	if(ec) {
+		Log(ec);
+		return;
+	}
+	ws.async_read(b1, [&b1](auto ec, int a){
+		OnReceive(b1, ec);
+	});
+}
+
+void OnHandshake(websocket::stream<boost::beast::tcp_stream> &ws, boost::asio::const_buffer &b, boost::beast::flat_buffer &b1, boost::system::error_code &ec) {
+	if(ec) {
+		Log(ec);
+		return;
+	}
+	ws.async_write(b, [&ws, &b1](auto ec, int a){
+		OnSend(ws, b1, ec);
+	});
+}
+
+void OnConnect(websocket::stream<boost::beast::tcp_stream> &ws, std::string &url, std::string &endpoint, boost::asio::const_buffer &b, boost::beast::flat_buffer &b1, boost::system::error_code &ec) {
+
+	if(ec) {
+		Log(ec);
+		return;
+	}
+	ws.async_handshake(url, endpoint, [&ws, &b, &b1](auto ec){
+		OnHandshake(ws, b, b1, ec);
+	});
+}
+
+
+void OnResolve(websocket::stream<boost::beast::tcp_stream> &ws, std::string &url, std::string &endpoint, boost::asio::const_buffer &b, boost::beast::flat_buffer &b1, boost::system::error_code &ec, tcp::resolver::iterator resolverIt) {
+	
+	if (ec) {
+		Log(ec);
+		return;
+	}
+	ws.next_layer().async_connect(*resolverIt, [&ws, &url, &endpoint, &b, &b1](auto ec){
+		OnConnect(ws, url, endpoint, b, b1, ec);
+	});
+	
+}
+
 int main() {
 
+	std::string url{"ltnm.learncppthroughprojects.com"};
+	std::string endpoint{"/echo"};
+	std::string port{"80"};
+	std::string message{"Hi"};
+
 	boost::asio::io_context ioc{};
-
-	tcp::socket socket{ioc};
-
-	boost::system::error_code ec{};
-	tcp::resolver resolver{ioc};
-	auto resolverIt{resolver.resolve("ltnm.learncppthroughprojects.com", "80", ec)};
-
-	if(ec) {
-		Log(ec);
-		return -1;
-	}
-	socket.connect(*resolverIt, ec);
-	if(ec) {
-		Log(ec);
-		return -2;
-	}
-	Log(ec);
-	websocket::stream<boost::beast::tcp_stream> ws(std::move(socket));
-	ws.handshake("ltnm.learncppthroughprojects.com", "/echo", ec);
-	Log(ec);
-	boost::asio::const_buffer b("ssup", sizeof("ssup"));
-	ws.write(b, ec);
-	Log(ec);
+	websocket::stream<boost::beast::tcp_stream> ws{ioc};
+	boost::asio::const_buffer b{message.c_str(), message.size()};
 	boost::beast::flat_buffer b1{};
-	ws.read(b1, ec);
-	std::cout << boost::beast::make_printable(b1.data()) << '\n';
+
+	tcp::resolver resolver{ioc};
+	resolver.async_resolve(url, port, [&ws, &url, &endpoint, &b, &b1](auto ec, auto resolverIt){
+		OnResolve(ws, url, endpoint, b, b1, ec, resolverIt);
+	});
+
+	ioc.run();
+
 	return 0;
 }
